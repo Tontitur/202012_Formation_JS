73dbdef85e7452c8ce6896eb95b25df373eee468
Tontitur <arthur.le.bris@live.fr>
2020-12-16 13:56:53 +0100

    Initialisation fonction et appel fonction

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // alert('le fichier est bien en place');
+// declaration d'une fonction
+function initialisationJS(){
 // On crée une variable et on affecte la valeur à DeviceOrientationEvent. 
 var balise=document.querySelector('#jsload');
 // modification du contenu html de la balise dans la var
 balise.style.backgroundColor="GREEN";
 // Modif du style CSS dans la varaiable
 balise.innerHTML="JS CHARGE"
+}
+// usage d'une fonction
+initialisationJS();
