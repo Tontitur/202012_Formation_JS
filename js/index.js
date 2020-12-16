d1bb677331d91f5be3c1e22dc98f61a50e495453
Tontitur <arthur.le.bris@live.fr>
2020-12-16 14:12:09 +0100

    Evenement onsubmit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 // alert('le fichier est bien en place');
 // declaration d'une fonction
-function initialisationJS(prenom){
-// On crée une variable et on affecte la valeur à DeviceOrientationEvent. 
-// Ajouter le prenom est un attribut que l'on définit
-var balise=document.querySelector('#jsload');
-// modification du contenu html de la balise dans la var
-balise.style.backgroundColor="GREEN";
-// Modif du style CSS dans la varaiable
-balise.innerHTML="JS CHARGE POUR "+prenom
-// on ajoute la concaténation de l'attribut prenom
+function initialisationJS(prenom) {
+    // On crée une variable et on affecte la valeur à DeviceOrientationEvent. 
+    // Ajouter le prenom est un attribut que l'on définit
+    var balise = document.querySelector('#jsload');
+    // modification du contenu html de la balise dans la var
+    balise.style.backgroundColor = "GREEN";
+    // Modif du style CSS dans la varaiable
+    balise.innerHTML = "JS CHARGE POUR " + prenom
+    // on ajoute la concaténation de l'attribut prenom
 }
 // usage d'une fonction
 initialisationJS('Arthur');
 // on définit que la variable prenom à pour cette initialisation la valeur 'Arthur'
+function formSubmited(params) {
+    console.log('Mon formulaire est "submit"')
+}
