a5a40de12c60cafd910c67d471fbc00e4b727cb0
Tontitur <arthur.le.bris@live.fr>
2020-12-17 14:41:10 +0100

    creation fonction put post et remove dans le json

--- js/rest.js
+++ js/rest.js
@@ -38,4 +38,38 @@ function post(ressourceUrl, ressource) {
     //transformation avec stingify du contenu Objet en JSON
     xhr.send(JSON.stringify(ressource));
 }
-get('/postit/1')
+
+/**
+ * Suppression d'une ressource sur ressourceUrl => Reprise de la fonction
+ * @param {*} ressourceUrl 
+ */
+function remove(ressourceUrl) {
+    var xhr=new XMLHttpRequest();
+    xhr.open('DELETE',BASE_URL+ressourceUrl);
+    xhr.onreadystatechange=function (evt){
+    if(evt.currentTarget.readyState < XMLHttpRequest.DONE){return;}
+    var objt=JSON.parse(evt.currentTarget.response);
+    console.log(objt);
+};
+xhr.send();
+}
+
+/**
+ * Mise à jour d'une ressource sur ressourceUrl
+ * @param {Uri} ressourceUrl le chemin d'accès à la ressource
+ * @param {Object} ressource data à envoyer
+ */
+function put(ressourceUrl, ressource) {
+    var xhr=new XMLHttpRequest();
+    xhr.open('PUT',BASE_URL+ressourceUrl);
+    //specification du type contenu
+    xhr.setRequestHeader('Content-Type','application/json')
+    //specification de ce qui est attendu en retour
+    xhr.setRequestHeader('Accept','application/json')
+    xhr.onreadystatechange=function (evt) {
+        if(xhr.readyState<4){return;}
+        console.log(JSON.parse(xhr.response));    
+    }
+    //transformation avec stingify du contenu Objet en JSON
+    xhr.send(JSON.stringify(ressource));
+}
